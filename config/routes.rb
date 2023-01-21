814aa75573a57652c93173a330c1d50d5deb5b1d
dn.artego <dn.artego@gmail.com>
2023-01-21 23:19:40 +0300

    form, layouts, assets pipeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'questions#index'
+
   resources :questions
   # post '/questions', to: 'questions#create'
   # patch '/questions/:id', to: 'questions#update'
