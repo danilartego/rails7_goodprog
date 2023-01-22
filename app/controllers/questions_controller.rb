cd6b99d0cbdefeac9d7e1887bbff06934d3905ac
dn.artego <dn.artego@gmail.com>
2023-01-22 22:26:05 +0300

    partials, helpers, flash

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,28 +5,32 @@ class QuestionsController < ApplicationController
   def create
     question = Question.create(question_params)
 
-    redirect_to question_path(question)
+    #flash[:notice] = 'Новый вопрос создан'
+
+    redirect_to question_path(question), notice: 'Новый вопрос создан'
   end
 
   def update
     #debugger
     @question.update(question_params)
 
-    redirect_to question_path(@question)
+    redirect_to question_path(@question), notice: 'Сохранили вопрос'
   end
 
   def destroy
     #debugger
     @question.destroy
 
-    redirect_to questions_path
+    redirect_to questions_path, notice: 'Вопрос удален'
   end
 
   def show
   end
 
   def index
+    @question = Question.new
     @questions = Question.all
+
   end
 
   def new
