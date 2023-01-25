1308ce0fb8e95cf05c5027e5531c1b81a1b965ac
dn.artego <dn.artego@gmail.com>
2023-01-25 15:51:36 +0300

    validates, callback, CRUD

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
 
+  before_save :downcase
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  def downcase
+    nickname.downcase!
+    email.downcase!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :questions
   resource :session, only: %i[new create destroy]
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update destroy]
   # post '/questions', to: 'questions#create'
   # patch '/questions/:id', to: 'questions#update'
   # delete '/questions/:id', to: 'questions#destroy'
